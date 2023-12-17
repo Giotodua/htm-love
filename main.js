8c29ba2b24804935ffe144d39a9d4dc9c6e0f44d
George Todua <85410682+Giotodua@users.noreply.github.com>
2023-12-17 21:36:54 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const allInps = document.querySelectorAll(".form-control");
+const inputOneValue = document.getElementById('inputOne');
+const inputTwoValue = document.getElementById('inputTwo');
+const btnCalculateValue = document.getElementById('btn-calculate');
+const resultDiv = document.getElementById('result-div');
+var contactCollection = [];
+btnCalculateValue.addEventListener("click", () => {
+    let percentage;
+    
+    if (inputOneValue.value == "" || inputTwoValue.value == "" ){
+        
+        console.log(inputOneValue)
+    } else {
+        percentage = Math.floor(Math.random() * 100 +
+         1);
+         resultDiv.innerHTML = `<h1>${percentage}% <h1>`;
+
+        //  setTimeout(() =>{
+        //     resultDiv.innerHTML = "";
+
+        //  }, 4000);
+         clearInputs()
+    }
+
+
+    inputOneValue.value = "";
+    inputTwoValue.value = "";
+})
+function clearInputs(){
+    allInps.forEach(item => {item.value = "";})
+}
+
+
+
+class contact {
+    inputOne;
+    inputTwo;
+   
+    constructor(inputOne, inputTwo,){
+        this.inputOne = inputOne;
+        this.inputTwo =  inputTwo;
+    }
+}
